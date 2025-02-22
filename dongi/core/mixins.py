a02b2de721e9e71e4701236b73228428ba7b5447
arshia <arshiarezagholi1212@gmail.com>
2025-02-22 20:08:32 +0330

    added reusable mixins and base view set

---
+++ dongi/core/mixins.py
@@ -0,0 +1,17 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class ListCacheResponseMixin:
+    pass
+
+
+class ThrottleMixin:
+    pass
+
+
+class LoggingMixin:
+    pass
+
+
+class DefaultPaginationMixin(PageNumberPagination):
+    pass
--- dongi/core/views.py
+++ dongi/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend, OrderingFilter
+from rest_framework import viewsets
+from core.mixins import ListCacheResponseMixin, ThrottleMixin, LoggingMixin, DefaultPaginationMixin
 
-# Create your views here.
+
+class BaseViewSet(ListCacheResponseMixin, ThrottleMixin, LoggingMixin, viewsets.ModelViewSet):
+    pagination_class = DefaultPaginationMixin
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
