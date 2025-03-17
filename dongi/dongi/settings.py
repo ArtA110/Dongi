87634cb7830ed9a3e3c301b320c4297b3856929a
ali.tabatabaeian16 <ali.tabatabaeian16@gmail.com>
2025-03-17 17:11:24 +0330

    DOC: added drf docs

--- dongi/dongi/settings.py
+++ dongi/dongi/settings.py
@@ -42,6 +42,8 @@ THIRD_PARTY_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
 ]
 
 LOCAL_APPS = [
@@ -173,8 +175,21 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.ScopedRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {'receives': '1000/day', 'uploads': '40/day'},
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "user.User"
+
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Dongi API',
+    'DESCRIPTION': 'An Expense Sharing app Powered by Django',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_DIST': 'SIDECAR',
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR', 
+}
--- dongi/dongi/urls.py
+++ dongi/dongi/urls.py
@@ -23,10 +23,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path('schema/file', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path(
         "api/v1/",
         include(
