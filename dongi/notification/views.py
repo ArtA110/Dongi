602686e0aa3653ed2fd7a6a030d3fdd126f7cc90
shahriar-fattahi <alifattahisf@gmail.com>
2025-03-19 18:13:41 +0330

    feat: implement permission system in querysets

--- dongi/notification/views.py
+++ dongi/notification/views.py
@@ -1,8 +1,17 @@
-from rest_framework import viewsets, permissions
+from rest_framework import permissions, viewsets
+
 from .models import Notification
 from .serializers import NotificationSerializer
 
 
 class NotificationViewSet(viewsets.ModelViewSet):
-    queryset = Notification.objects.select_related('user')
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    queryset = Notification.objects.select_related("user")
     serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return self.queryset
+        return self.queryset.filter(user=self.request.user)
