aeece79497606fa1af6d609677c2435b1cfba9d4
ali.tabatabaeian16 <ali.tabatabaeian16@gmail.com>
2025-04-10 14:07:00 +0330

    FIX: fixed user serializer for posting info

--- dongi/dongi/settings.py
+++ dongi/dongi/settings.py
@@ -186,6 +186,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.ScopedRateThrottle',
--- dongi/user/serializers.py
+++ dongi/user/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        exclude = ['password']
+        fields = "__all__"
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -17,6 +17,23 @@ class UserSerializer(serializers.ModelSerializer):
             for field_name in list(self.fields.keys()):
                 if field_name not in allowed_fields:
                     self.fields.pop(field_name)
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop('password', None)
+        return representation
+    
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
 
 
 class GroupSerializer(serializers.ModelSerializer):
